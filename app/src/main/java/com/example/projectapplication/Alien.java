936dc3776ed412c83de065882cd71da9ff91c112
Kathleen Edie <kathleen.edie@arnoldclark.com>
2022-03-19 18:08:07 +0000

    random package added

--- app/src/main/java/com/example/projectapplication/Alien.java
+++ app/src/main/java/com/example/projectapplication/Alien.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.RectF;
+import java.util.Random;
 
 public class Alien {
 
